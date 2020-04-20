d8a959674fb2f395de1bc3ba46f71df2a7560aba
Ambre <ambre_donnadieu@live.fr>
2020-04-20 20:00:06 +0200

    Tutoriel part 2

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,20 +1,30 @@
-  
+
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using System.Threading;
 
 class Receive
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
         var factory = new ConnectionFactory() { HostName = "localhost" };
-        using(var connection = factory.CreateConnection())
-        using(var channel = connection.CreateModel())
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
             Console.WriteLine(" [*] Waiting for messages.");
+
+            // var consumer = new EventingBasicConsumer(channel);
+            // consumer.Received += (model, ea) =>
+            // {
+            //     var body = ea.Body;
+            //     var message = Encoding.UTF8.GetString(body.ToArray());
+            //     Console.WriteLine(" [x] Received {0}", message);
+            // };
+            // channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
 
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
@@ -22,8 +32,13 @@ class Receive
                 var body = ea.Body;
                 var message = Encoding.UTF8.GetString(body.ToArray());
                 Console.WriteLine(" [x] Received {0}", message);
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
             };
-            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+            channel.BasicConsume(queue: "task_queue", autoAck: true, consumer: consumer);
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
--- Send/Send.cs
+++ Send/Send.cs
@@ -5,30 +5,52 @@ using System.Text;
 
 class Send
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
         var factory = new ConnectionFactory() { HostName = "localhost" };
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
-            channel.QueueDeclare(queue: "hello",
-                                 durable: false,
+
+            // channel.QueueDeclare(queue: "hello",
+            //                      durable: false,
+            //                      exclusive: false,
+            //                      autoDelete: false,
+            //                      arguments: null);
+
+            // string message = "Hello World!";
+            // var body = Encoding.UTF8.GetBytes(message);
+
+            // channel.BasicPublish(exchange: "",
+            //                      routingKey: "hello",
+            //                      basicProperties: null,
+            //                      body: body);
+            // Console.WriteLine(" [x] Sent {0}", message);
+
+            channel.QueueDeclare(queue: "task_queue",
+                                 durable: true,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
 
-            string message = "Hello World!";
+            var message = GetMessage(args);
             var body = Encoding.UTF8.GetBytes(message);
 
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
             channel.BasicPublish(exchange: "",
-                                 routingKey: "hello",
-                                 basicProperties: null,
+                                 routingKey: "task_queue",
+                                 basicProperties: properties,
                                  body: body);
-            Console.WriteLine(" [x] Sent {0}", message);
         }
 
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
+    }
+    private static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
     }
 }
 
